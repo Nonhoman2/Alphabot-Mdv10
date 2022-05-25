1a4064c44af4dc184f4e174acbf7d98b84de85cb
Nonhoman2 <106153671+Nonhoman2@users.noreply.github.com>
2022-05-25 16:41:25 +0800

    settings.js

--- settings.js
+++ settings.js
@@ -3,12 +3,32 @@ const chalk = require('chalk')
 
 // Website Api
 global.APIs = {
-	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan le owner
+	alfa: 'https://api.zeeoneofc.xyz',
+        nrtm: 'https://nurutomo.herokuapp.com',
+        bg: 'http://bochil.ddns.net',
+        xteam: 'https://api.xteam.xyz',
+        zahir: 'https://zahirr-web.herokuapp.com',
+        zeks: 'https://api.zeks.me',
+        pencarikode: 'https://pencarikode.xyz',
+        LeysCoder: 'https://leyscoders-api.herokuapp.com',
+        neoxr: 'https://neoxr-api.herokuapp.com',
+        amel: 'https://melcanz.com',
+        hardianto: 'https://hardianto.xyz',
+        lol: 'https://api.lolhuman.xyz' //apabila link api eror, segera laporkan le owner
 }
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'your apikey',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz': 'your apikey',
+        'https://neoxr-api.herokuapp.com': 'yntkts',
+        'https://api.xteam.xyz': 'apikeylu',
+        'https://melcanz.com': 'apikeylu',
+        'https://api.lolhuman.xyz': 'apikeylu',
+        'https://zahirr-web.herokuapp.com': 'zahirgans',
+        'https://api.zeks.me': 'apikeylu',
+        'https://pencarikode.xyz': 'apikeylu',
+        'https://hardianto.xyz': 'hardianto',
+        'https://leyscoders-api.herokuapp.com': 'apikeylu'  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
